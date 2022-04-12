aa4e757decd0ee4087bb23df792e2cf73086fc34
devthalal <thalal.hassan@neoito.com>
2022-04-12 14:19:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
+import fs from "fs";
+import path from "path";
+import { getBody, getDB, sendResponse, shieldApi } from "./utils.js";
 
-    import fs from 'fs'
-    import path from 'path'
-// For testing take pull from Appblox/node-blox-sdk and npm install from path
-// import { env } from 'node-blox-sdk'
-// env.init()
-
+/**
+ * Update Candidate request hanlder
+ * @param {*} req
+ * @param {*} res
+ */
 const updateCandidate = async (req, res) => {
   try {
-    // const DB_FILE = path.resolve(process.env.DB_FILE_PATH)
-    // const inmemDB = getDB(DB_FILE)
-    // const newId = new Date().getTime()
-    // const newItem = await getBody(req)
-    // const newEntry = { id: newId, item: newItem }
-    // console.log('Request to add -', newItem)
-    // inmemDB.push(newEntry)
-    // fs.writeFileSync(DB_FILE, JSON.stringify(inmemDB))
-    // console.log('Updated DB:
-', inmemDB)
-    // console.log('
-')
-    sendResponse(res, 200, "Hello from updateCandidate")
+    const DB_FILE = path.resolve("../localdb.json");
+    const localDBData = getDB(DB_FILE);
+    const { id, ...updateData } = await getBody(req);
+
+    const index = localDBData.findIndex((obj) => {
+      return obj.id == id;
+    });
+
+    if (index !== -1) {
+      localDBData[index] = {
+        ...localDBData[index],
+        ...updateData,
+      };
+
+      fs.writeFileSync(DB_FILE, JSON.stringify(localDBData));
+      sendResponse(res, 200, { status: true, msg: "success" });
+    } else {
+      sendResponse(res, 400, { status: false, msg: "Cadidate not found" });
+    }
   } catch (e) {
-    console.log(e)
-    sendResponse(res, 500, { status: 'failed', errMsg: e.message })
+    sendResponse(res, 500, { status: false, msg: e.message, err: e });
   }
-}
+};
+
+export default { updateCandidate };
